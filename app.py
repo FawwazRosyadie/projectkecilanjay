ca4ce574586242543fbf75ec095ca43933b6585a
FawwazRosyadie <140362833+FawwazRosyadie@users.noreply.github.com>
2025-01-24 19:13:35 +0800

    backedn

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from weightligting import *
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/calculate', methods=['POST'])
 def calculate():
